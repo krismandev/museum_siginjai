3dc10f81195deb0a99f79073c2b30549a08f23a6
krismandev <krismanpratama@gmail.com>
2022-09-16 10:50:45 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get("/login","AuthController@login")->name("login");
+
+Route::post("/login","AuthController@postLogin")->name("postLogin");
+Route::get("/logout","AuthController@logout")->name("logout");
+
+
+Route::group(['middleware' => ['auth'],'prefix'=>'dashboard'], function(){
+    Route::get("/","Dashboard\DashboardController@index")->name("admin.dashboard.index");
+});
